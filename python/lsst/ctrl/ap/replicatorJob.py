f3a353fbff7d827e056ff4b3715487f6a7303ef2
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2014-05-02 17:34:47 -0500

    Added replicator job that can filter an sequence tags and exposure sequence ID

---
+++ python/lsst/ctrl/ap/replicatorJob.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+
+import os
+import sys
+import argparse
+import lsst.ctrl.events as events
+from lsst.daf.base import PropertySet
+
+class ReplicatorJob(object):
+
+    def __init__(self, raft, expectedSequenceTag, expectedExpSeqID):
+        # TODO:  these need to be placed in a configuration file
+        # which is loaded, so they are not embedded in the code
+        self.brokerName = "lsst8.ncsa.illinois.edu"
+        self.eventTopic = "ocs_startReadout"
+        self.raft = raft
+        self.expectedSequenceTag = expectedSequenceTag
+        self.expectedExpSeqID = expectedExpSeqID
+
+    def handleEvents(self):
+        eventSystem = events.EventSystem().getDefaultEventSystem()
+        eventSystem.createReceiver(self.brokerName, self.eventTopic)
+        while True:
+            print "listening on %s " % self.eventTopic
+            ocsEvent = eventSystem.receiveEvent(self.eventTopic)
+            ps = ocsEvent.getPropertySet()
+            imageID = ps.get("imageID")
+            # TODO:  for now, assume visit id, and exp. seq. id is also sent
+            sequenceTag = ps.get("sequenceTag")
+            exposureSequenceID = ps.get("exposureSequenceID")
+            print "image id = %s" % imageID
+            print "sequence tag = %s" % sequenceTag
+            print "exposure sequence id = %s" % exposureSequenceID
+            # TODO:  this should be done through a selector on the broker
+            # so we only get the sequenceTag and exp seq ID we are looking
+            # for.   Create a new Event object so we can filter it that way.
+            if sequenceTag == self.expectedSequenceTag and exposureSequenceID == self.expectedExpSeqID:
+                print "got expected info.  Getting image"
+                self.getCameraImage(imageID, sequenceTag, exposureSequenceID)
+
+    def getCameraImage(self, imageID, sequenceTag, exposureSequenceID):
+        print "getting Camera image for image id = %s, sequenceTag = %s, exposureSequenceID = %s" % (imageID, sequenceTag, exposureSequenceID)
+
+if __name__ == "__main__":
+    basename = os.path.basename(sys.argv[0])
+    parser = argparse.ArgumentParser(prog=basename)
+    parser.add_argument("-r", "--raft", type=int, action="store", help="raft number", required=True)
+    parser.add_argument("-t", "--sequenceTag", type=int, action="store", help="sequence Tag", required=True)
+    parser.add_argument("-x", "--exposureSequenceID", type=int, action="store", help="exposure sequence id", required=True)
+
+    args = parser.parse_args()
+    base = ReplicatorJob(args.raft, args.sequenceTag, args.exposureSequenceID)
+    base.handleEvents()
