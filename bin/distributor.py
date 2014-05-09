9b8315020d2b5c09a9455a25b67986a2f6a485a8
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2014-05-09 16:23:58 -0500

    Add REUSEADDR to allow rebinding on the same port after the death of the connection.

--- bin/distributor.py
+++ bin/distributor.py
@@ -53,6 +53,7 @@ class Distributor(object):
         self.port = port
         self.client = None
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((socket.gethostname(), self.port))
         self.sock.listen(5)
 
