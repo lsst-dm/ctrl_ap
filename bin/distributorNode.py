d40e34c94ffce980a8110d09faa81ff7f5f55e63
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2014-07-23 16:07:21 -0500

    Added code to handling incoming broadcast requests for distributor information from the archive dmcs, so the archive dmcs can repopulate itself with any information from the distributors

--- bin/distributorNode.py
+++ bin/distributorNode.py
@@ -34,6 +34,42 @@ from lsst.ctrl.ap.distributorHandler import DistributorHandler
 from lsst.ctrl.ap.jsonSocket import JSONSocket
 from lsst.pex.logging import Log
 
+class DistributorEventHandler(threading.Thread):
+    def __init__(self, dataTable, condition):
+        threading.Thread.__init__(self)
+        self.dataTable = dataTable
+        self.condition = condition
+        self.brokerName = "lsst8.ncsa.illinois.edu"
+        self.eventTopic = "archive_event"
+
+    def run(self):
+        eventSystem = events.EventSystem().getDefaultEventSystem()
+        eventSystem.createReceiver(self.brokerName, self.eventTopic)
+        while True:
+            self.logger.log(Log.INFO, "listening on %s " % self.eventTopic)
+            archiveEvent = eventSystem.receiveEvent(self.eventTopic)
+            ps = archiveEvent.getPropertySet()
+            archiveEventType = ps.get("archive_event")
+
+            eventSystem.createTransmitter(self.brokerName, "distributor_event")
+            root = PropertySet()
+            # todo: switch in event "request"
+            
+            # send events for contents dataTable
+            self.condition.aquire()
+            for key in dataTable:
+                elem = split(":")
+                root.add("exposureSequenceID", elem[0])
+                root.add("visitID", elem[1])
+                root.add("raft", elem[2])
+                net = dataTable[key]
+                root.add("networkAddress", net[0])
+                root.add("networkPort", net[1])
+                event = events.Event("distributor", "root")
+                eventSystem.publishEvent("distributor_event", event)
+            self.condition.release()
+                
+
 class DistributorNode(Node):
 
     def __init__(self, port):
@@ -45,6 +81,11 @@ class DistributorNode(Node):
     def activate(self):
         dataTable = {}
         condition = threading.Condition()
+
+        # start handler for incoming requests from the archive
+        eventHandler = DistributorEventHandler(dataTable, condition)
+        eventHandler.start()
+
         while True:
             self.logger.log(Log.INFO, "Waiting on connection")
             (client, (ipAddr, clientPort)) = self.inSock.accept()
