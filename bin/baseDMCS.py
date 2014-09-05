a70ee22baaeffafb87b1eabf97017a5f2b427326
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2014-09-05 14:29:03 -0500

    create heartbeat monitor thread that handles heartbeat connections and role of baseDMCS (main or failover)

--- bin/baseDMCS.py
+++ bin/baseDMCS.py
@@ -56,8 +56,7 @@ class BaseDMCS(object):
         self.MAIN = "main"
         self.FAILOVER = "failover"
         self.UNKNOWN = "unknown"
-        self.identity = self.UNKNOWN
-        self.establishInitialIdentity()
+        self.identity = [self.UNKNOWN]
 
     def loadConfig(self):
         apCtrlPath = envString.resolve("$CTRL_AP_DIR")
@@ -69,9 +68,9 @@ class BaseDMCS(object):
     def establishInitialIdentity(self):
         thisHost = socket.gethostname()
         if thisHost == self.baseConfig.main.host:
-            self.identity = self.MAIN
+            self.identity[0] = self.MAIN
         elif thisHost == self.baseConfig.failover.host:
-            self.identity = self.FAILOVER
+            self.identity[0] = self.FAILOVER
         else:
             print "couldn't determine host type from config"
             print "I think I'm: ",socket.gethostname()
@@ -79,86 +78,16 @@ class BaseDMCS(object):
             print "failover host is configured as: ",self.baseConfig.failover.host
             sys.exit(1)
 
-    def connectToFailover(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((self.baseConfig.failover.host, self.baseConfig.failover.heartbeatPort))
-        except socket.gaierror, err:
-            print "gaierror ", err
-            s = None
-        except socket.error, err:
-            print "socket.error ", err
-            s = None
-        return s
-
-    def establishConnection(self):
-        if self.identity == self.MAIN:
-            s = None
-            while s is None:
-                s = self.connectToFailover()
-                time.sleep(1)
-            self.isConnected = True
-            self.jsock = JSONSocket(s)
-
-            msg = {"msgtype":"inquire", "question":"whoareyou"}
-            self.jsock.sendJSON(msg)
-
-            # response is {"msgtype":"response", "answer":"failover|main"}
-            # where "answer" is either "failover" or "main"
-            msg = self.jsock.recvJSON()
-
-            print "other side is",msg["answer"]
-
-            if msg["answer"] == self.MAIN:
-                self.identity = self.FAILOVER
-
-        elif self.identity == self.FAILOVER:
-            serverSock = socket.socket()
-            serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            serverSock.bind((self.baseConfig.failover.host, self.baseConfig.failover.heartbeatPort))
-            serverSock.listen(5)
-            readable = False
-            while not readable:
-                readList = [ serverSock ]
-                timeout = 5
-                readable, writeable, errored = select.select(readList, [], [], 5)
-                if not readable:
-                    self.identity = self.MAIN
-                    print "main didn't contact; switching to MAIN"
-                    continue
-            (clientSock, (ipAddr, clientPort)) = serverSock.accept()
-            print "connection accepted!"
-            self.isConnected = True
-            self.jsock = JSONSocket(clientSock)
-
-            msg = self.jsock.recvJSON()
-            # TODO: check message
-            if msg["msgtype"] != "inquire":
-                print "unknown message = ",msg
-                return
-            if msg["question"] != "whoareyou":
-                print "unknown message = ",msg
-                return
-            resp = {"msgtype":"response", "answer":self.identity}
-            msg = self.jsock.sendJSON(resp)
-
     def handleEvents(self):
+        self.establishInitialIdentity()
+
         eventSystem = events.EventSystem().getDefaultEventSystem()
         eventSystem.createReceiver(self.brokerName, self.eventTopic)
         st = Status()
         st.publish(st.baseDMCS, st.start)
 
-        self.establishConnection()
-
-        if self.identity == self.MAIN:
-            baseHeartEvent = threading.Event()
-            hb = BaseHeartbeat(self.jsock, baseHeartEvent)
-            hb.start()
-        elif self.identity == self.FAILOVER:
-            self.event = threading.Event()
-            hbr = BaseHeartbeatHandler(self.jsock, self.event)
-            hbr.start()
-
+        hm = HeartbeatMonitor(self.baseConfig, self.identity)
+        hm.start()
 
         while True:
             self.logger.log(Log.INFO, "listening on %s " % self.eventTopic)
@@ -187,6 +116,99 @@ class BaseDMCS(object):
                 st.publish(st.baseDMCS, st.receivedMsg, {ocsEventType:data})
                 jm = jobManager.JobManager()
                 jm.submitWorkerJobs(visitID, exposures, boresight, filterID)
+
+class HeartbeatMonitor(threading.Thread):
+    def __init__(self, baseConfig, identity):
+        super(HeartbeatMonitor, self).__init__()
+        self.baseConfig = baseConfig
+        self.identity = identity
+        self.MAIN = "main"
+        self.FAILOVER = "failover"
+        self.UNKNOWN = "unknown"
+
+    def run(self):
+        while True:
+            self.establishConnection()
+            thr = self.startHeartbeat()
+            thr.join()
+
+    def startHeartbeat(self):
+        if self.identity[0] == self.MAIN:
+            baseHeartEvent = threading.Event()
+            hb = BaseHeartbeat(self.jsock, baseHeartEvent)
+            hb.start()
+            return hb
+        elif self.identity[0] == self.FAILOVER:
+            self.event = threading.Event()
+            hbr = BaseHeartbeatHandler(self.jsock, self.event)
+            hbr.start()
+            return hbr
+
+
+    def connectToFailover(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((self.baseConfig.failover.host, self.baseConfig.failover.heartbeatPort))
+        except socket.gaierror, err:
+            print "gaierror ", err
+            s = None
+        except socket.error, err:
+            print "socket.error ", err
+            s = None
+        return s
+
+
+    def establishConnection(self):
+        if self.identity[0] == self.MAIN:
+            s = None
+            while s is None:
+                s = self.connectToFailover()
+                time.sleep(1)
+            self.isConnected = True
+            self.jsock = JSONSocket(s)
+
+            msg = {"msgtype":"inquire", "question":"whoareyou"}
+            self.jsock.sendJSON(msg)
+
+            # response is {"msgtype":"response", "answer":"failover|main"}
+            # where "answer" is either "failover" or "main"
+            msg = self.jsock.recvJSON()
+
+            print "other side is",msg["answer"]
+
+            if msg["answer"] == self.MAIN:
+                self.identity[0] = self.FAILOVER
+
+        elif self.identity[0] == self.FAILOVER:
+            serverSock = socket.socket()
+            serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            serverSock.bind((self.baseConfig.failover.host, self.baseConfig.failover.heartbeatPort))
+            serverSock.listen(5)
+            readable = False
+            while not readable:
+                readList = [ serverSock ]
+                timeout = 5
+                readable, writeable, errored = select.select(readList, [], [], 5)
+                if not readable:
+                    self.identity[0] = self.MAIN
+                    print "main didn't contact; switching to MAIN"
+                    continue
+            (clientSock, (ipAddr, clientPort)) = serverSock.accept()
+            print "connection accepted!"
+            self.isConnected = True
+            self.jsock = JSONSocket(clientSock)
+
+            msg = self.jsock.recvJSON()
+            # TODO: check message
+            if msg["msgtype"] != "inquire":
+                print "unknown message = ",msg
+                return
+            if msg["question"] != "whoareyou":
+                print "unknown message = ",msg
+                return
+            resp = {"msgtype":"response", "answer":self.identity[0]}
+            msg = self.jsock.sendJSON(resp)
+
 
 class BaseHeartbeat(threading.Thread):
     def __init__(self, jsock, event):
